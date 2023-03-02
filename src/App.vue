e54d44aecf261515b096cebbe78ae8188f8e9153
AdeDevs <adeyemiakinyemi01@gmail.com>
2023-03-02 12:51:55 +0100

    Updated

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HomePage from './components/HomePage.vue';
 export default {
   name: 'App',
   components: {
-    Home: HomePage
+      HomePage
 }
 }
 </script>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -6,7 +6,7 @@
 
 <script>
 export default {
-    name: TestingHomePage,
+    name: HomePage,
     props: {
         msg: String
     }
