1b71336e9d09f47bd403496b97f61904f51fd47d
AdeDevs <adeyemiakinyemi01@gmail.com>
2023-03-04 12:37:36 +0100

    Updated

--- src/components/TutorialNav.vue
+++ src/components/TutorialNav.vue
@@ -1,11 +1,33 @@
 <template>
-    <div>
-        <h1>Lmao</h1>
-    </div>
+    <header>
+        <nav>
+            <a href="#" class="nav-branding">ADEYEMI</a>
+
+            <ul class="nav-menu">
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">About</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Projects</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
 </template>
 
 <script>
 export default {
     name: "TutorialNav"
 }
+alert("Been on this for a month")
 </script>
+
+<style>
+@import '../styles/tutorial.css'
+</style>
