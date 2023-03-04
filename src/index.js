aee9cca6cba7f2c99410ead75c147f1206944863
AdeDevs <adeyemiakinyemi01@gmail.com>
2023-03-04 12:47:01 +0100

    Updated

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBBBVhB8ejXXibBW4b1kVZ-Tr1N4HRHNFc",
+  authDomain: "kryptykhamburger.firebaseapp.com",
+  projectId: "kryptykhamburger",
+  storageBucket: "kryptykhamburger.appspot.com",
+  messagingSenderId: "736986579994",
+  appId: "1:736986579994:web:0b209d7dc3a9214da2fda3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
